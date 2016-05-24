17b899a7140f515c439dccabc48b1d2ef1e9d289
Preston Stosur-Bassett <preston@stosur.info>
2016-05-24 02:16:32 -0400

    added helloworld project

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+/*
+* @Author: Preston Stosur-Bassett http://stosur.info
+* @Author: 王宁
+* @Date: May 24, 2016
+* @Description: HelloWorld Introduction to Java
+*/
+
+// TODO: Finish the rest of this file, and make it output to the command line (terminal window) `Hello World!`

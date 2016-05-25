25d7ec1e9a73dc93523116931193e7e89b98799f
ilikeyoucallmening <angelwang820@163.com>
2016-05-25 21:11:57 +0800

    finished writing FirstVariable class

---
+++ FirstVariable.java
@@ -0,0 +1,10 @@
+class FirstVariale
+{
+  public static void main (String[] args)
+  {
+    String message="Initial value";
+    System.out.putln(message);
+    message="Modified value";
+    System.out.println(message);
+  }
+}

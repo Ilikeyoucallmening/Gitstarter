b4f74140ae03791b9dffde24b86b223911fc96ed
Preston Stosur-Bassett <preston@stosur.info>
2016-05-24 22:30:33 -0400

    corrected 2 code errors

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -7,8 +7,8 @@
 
 class HelloWorld
 {
-  public static void main (String[]args)
+  public static void main (String[] args)
   {
-    System.out.println("Hellow World!");
+    System.out.println("Hello World!");
   }
-} 
+}
